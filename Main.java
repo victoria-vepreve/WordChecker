16e40a5fee4dc9f447f40f4b6dee8e4aedfca805
victoria-vepreve <victoriav88@nycstudents.net>
2024-10-23 09:29:19 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -5,15 +5,39 @@ public class Main
     public static void main(String[] args)
     {
         ArrayList<String> words = new ArrayList<String>();
-
         words.add("an");
         words.add("band");
         words.add("band");
         words.add("abandon");
+        WordChecker x = new WordChecker(words);
+        System.out.println(x.isWordChain());
 
+
+        ArrayList<String> words2 = new ArrayList<String>();
+        words2.add("to");
+        words2.add("too");
+        words2.add("stool");
+        words2.add("tools");
+        System.out.println(words2);
+        WordChecker y = new WordChecker(words2);
+        System.out.println(y);
+        System.out.println(y.isWordChain());
+
+        ArrayList<String> words3 = new ArrayList<String>();
+        words3.add("catch");
+        words3.add("bobcat");
+        words3.add("catchacat");
+        words3.add("cat");
+        words3.add("at");
+        WordChecker z = new WordChecker(words3);
+        System.out.println(z.createList("cat"));
+        System.out.println(z.createList("catch"));
+        System.out.println(z.createList("dog"));
         
 
-        WordChecker x = new WordChecker(words);
+
+
+
 
         
     }
--- WordChecker.java
+++ WordChecker.java
@@ -7,12 +7,13 @@ public class WordChecker
     
     public boolean isWordChain() 
     {
-        boolean wordChain = true;
+        // boolean wordChain = true;
         for(int i = 1; i < wordList.size(); i++)
         {
             String after = wordList.get(i);
             String before = wordList.get(i-1);
             if(after.indexOf(before) < 0) return false;
+
         }
         return true;
     }
@@ -37,9 +38,13 @@ public class WordChecker
     {
         wordList = list;
 
-        wordList = new ArrayList<String>();
+        //wordList = new ArrayList<String>();
 
         
+    }
+    public String toString()
+    {
+        return wordList.toString();
     }
 
 
